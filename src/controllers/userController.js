6eff557e30d26e75ad364f69da1fd8c1f0fab221
lavi <165945697+scriptlver@users.noreply.github.com>
2026-03-02 19:25:47 -0300

    silhueta - metade do login

--- src/controllers/userController.js
+++ src/controllers/userController.js
@@ -1,4 +1,9 @@
-const { createUserService, getUsersService } = require("../services/userService");
+const {
+  createUserService,
+  getUsersService,
+} = require("../services/userService");
+const jwt = require("jsonwebtoken");
+const User = require("../models/User");
 
 async function createUser(req, res, next) {
   try {
@@ -41,8 +46,49 @@ async function deleteUser(req, res, next) {
   }
 }
 
+async function login(req, res, next) {
+  try {
+    const { email, password } = req.body;
+
+    const user = await User.findOne({ email }).select("+password");
+
+    if (!user) {
+      return res.status(401).json({ message: "Email ou senha inválidos" });
+    }
+
+    const isMatch = await user.comparePassword(password);
+
+    if (!isMatch) {
+      return res.status(401).json({ message: "Email ou senha inválidos" });
+    }
+
+    // gerar token
+    const token = jwt.sign(
+      {
+        id: user._id,
+        role: user.role,
+      },
+      process.env.JWT_SECRET,
+      { expiresIn: "1d" },
+    );
+
+    return res.status(200).json({
+      token,
+      user: {
+        id: user._id,
+        name: user.name,
+        email: user.email,
+        role: user.role,
+      },
+    });
+  } catch (error) {
+    next(error);
+  }
+}
+
 module.exports = {
   createUser,
   getUsers,
   deleteUser,
+  login,
 };
--- src/routes/userRoutes.js
+++ src/routes/userRoutes.js
@@ -1,21 +1,25 @@
 const express = require("express");
 
-const { createUser, getUsers, deleteUser } = require("../controllers/userController");
+const {
+  createUser,
+  getUsers,
+  deleteUser,
+  login,
+} = require("../controllers/userController");
+
 const authMiddleware = require("../middlewares/authMiddleware");
 const roleMiddleware = require("../middlewares/roleMiddleware");
 
 const router = express.Router();
 
-
+// criar usuário
 router.post("/", createUser);
 
-router.get(
-  "/",
-  authMiddleware,
-  roleMiddleware("admin"),
-  getUsers
-);
+// login
+router.post("/login", login); // nn sei se isso ta certo nn kkkkkkkkkk
 
-router.delete("/:id", deleteUser);
+router.get("/", authMiddleware, roleMiddleware("admin"), getUsers);
+
+router.delete("/:id", authMiddleware, roleMiddleware("admin"), deleteUser);
 
 module.exports = router;

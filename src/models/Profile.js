184b5229bb99adeec1a5385cb16c8a72814ca3fb
Ana Luisa <anadrvv@gmail.com>
2026-03-04 14:35:58 -0300

    profile model

--- src/models/Profile.js
+++ src/models/Profile.js
@@ -1,7 +1,66 @@
 const mongoose = require("mongoose");
 
 const ProfileSchema = new mongoose.Schema(
-    {}
+  {
+    user: {
+      type: mongoose.Schema.Types.ObjectId,
+      ref: "User",
+      required: true,
+      unique: true,
+    },
+
+    username: {
+      type: String,
+      unique: true,
+      required: true,
+      trim: true,
+    },
+
+    bio: {
+      type: String,
+      maxlength: 300,
+      default: "",
+    },
+
+    isPrivate: {
+      type: Boolean,
+      default: false,
+    },
+
+    favorites: [
+      {
+        type: mongoose.Schema.Types.ObjectId,
+        ref: "Item",
+      },
+    ],
+
+    favoriteOfMonth: {
+      type: mongoose.Schema.Types.ObjectId,
+      ref: "Item",
+      default: null,
+    },
+
+    pinnedReview: {
+      type: mongoose.Schema.Types.ObjectId,
+      ref: "Review",
+      default: null,
+    },
+
+    followers: [
+      {
+        type: mongoose.Schema.Types.ObjectId,
+        ref: "User",
+      },
+    ],
+
+    following: [
+      {
+        type: mongoose.Schema.Types.ObjectId,
+        ref: "User",
+      },
+    ],
+  },
+  { timestamps: true }
 );
 
 module.exports = mongoose.model("Profile", ProfileSchema);

8896e07aaa3a599b2ec9ced8613dafb4c43c9cfe
Ana Luisa <anadrvv@gmail.com>
2026-03-07 13:09:56 -0300

    defini as funções

---
+++ src/controllers/ratingController.js
@@ -0,0 +1,62 @@
+const Rating = require("../models/Rating");
+
+async function createRating(data) {
+    try{
+
+    } catch (err) {
+        throw err;
+    }
+}
+
+async function getAllRatings() {
+
+}
+
+async function getRatingsByUser(user) {
+
+}
+
+async function getRatingsByItem(item) {
+
+}
+
+async function getRatingsByType(type) {
+
+}
+
+async function getRatingById(id) {
+
+}
+
+async function getTopRatings() {
+
+}
+
+async function getRecentRatings() {
+
+}
+
+async function getTredingRatings() {
+
+}
+
+async function deleteRating(id) {
+
+}
+
+async function updateRating(id) {
+
+}
+
+module.exports = {
+    createRating, 
+    getAllRatings,
+    getRatingsByUser,
+    getRatingsByItem,
+    getRatingById,
+    getRecentRatings,
+    getTopRatings,
+    getTredingRatings,
+    deleteRating,
+    updateRating,
+};

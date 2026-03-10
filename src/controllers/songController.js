dfbb3c64baa301b0097a759289182c32385e4723
lavi <165945697+scriptlver@users.noreply.github.com>
2026-03-10 20:49:35 -0300

    tratamento de erro song

--- src/controllers/songController.js
+++ src/controllers/songController.js
@@ -5,47 +5,100 @@ async function createSong(data) {
   try {
     const song = await Song.create(data);
     return song;
-  } catch (err) {
-    throw err;
+  } catch (error) {
+    throw new Error("Erro ao criar música");
   }
 }
 
 // listar todas
 async function getAllSongs() {
-  return await Song.find().sort({ createdAt: -1 });
+  try {
+    return await Song.find().sort({ createdAt: -1 });
+  } catch (error) {
+    throw new Error("Erro ao buscar músicas");
+  }
 }
 
 // listar por gênero
 async function getSongsByGenre(genre) {
-  return await Song.find({
-    genre: genre.toLowerCase(),
-  }).sort({ createdAt: -1 });
+  try {
+    const songs = await Song.find({
+      genre: genre.toLowerCase(),
+    }).sort({ createdAt: -1 });
+
+    if (!songs || songs.length === 0) {
+      throw new Error("Nenhuma música encontrada para esse gênero");
+    }
+
+    return songs;
+  } catch (error) {
+    throw new Error("Erro ao buscar músicas por gênero");
+  }
 }
 
 // listar por artista
 async function getSongsByArtist(artist) {
-  return await Song.find({
-    artist: { $regex: new RegExp(`^${artist}$`, "i") },
-  }).sort({ createdAt: -1 });
+  try {
+    const songs = await Song.find({
+      artist: { $regex: new RegExp(`^${artist}$`, "i") },
+    }).sort({ createdAt: -1 });
+
+    if (!songs || songs.length === 0) {
+      throw new Error("Nenhuma música encontrada para esse artista");
+    }
+
+    return songs;
+  } catch (error) {
+    throw new Error("Erro ao buscar músicas por artista");
+  }
 }
 
 // buscar por id
 async function getSongById(id) {
-  return await Song.findById(id);
+  try {
+    const song = await Song.findById(id);
+
+    if (!song) {
+      throw new Error("Música não encontrada");
+    }
+
+    return song;
+  } catch (error) {
+    throw new Error("Erro ao buscar música");
+  }
 }
 
 // atualizar
 async function updateSong(id, data) {
-  return await Song.findByIdAndUpdate(id, data, {
-    new: true,
-    runValidators: true,
-  });
+  try {
+    const song = await Song.findByIdAndUpdate(id, data, {
+      new: true,
+      runValidators: true,
+    });
+
+    if (!song) {
+      throw new Error("Música não encontrada");
+    }
+
+    return song;
+  } catch (error) {
+    throw new Error("Erro ao atualizar música");
+  }
 }
 
 // deletar
 async function deleteSong(id) {
-  await Song.findByIdAndDelete(id);
-  return "Música deletada com sucesso";
+  try {
+    const song = await Song.findByIdAndDelete(id);
+
+    if (!song) {
+      throw new Error("Música não encontrada");
+    }
+
+    return "Música deletada com sucesso";
+  } catch (error) {
+    throw new Error("Erro ao deletar música");
+  }
 }
 
 module.exports = {

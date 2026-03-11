d8e87bc41c52f48bc356989d5c4b4b2e9d2642d5
Sabrina Vitoria <sabrinavitoria24@gmail.com>
2026-03-11 19:07:21 -0300

    tratamento de erros de filmes

--- src/controllers/movieController.js
+++ src/controllers/movieController.js
@@ -6,39 +6,86 @@ async function createMovie(data) {
     const movie = await Movie.create(data);
     return movie;
  }catch (err) {
-    throw err; 
+    throw new Error("Erro ao criar filme");  
   }
 }
 
 // listar todas 
 async function getAllMovie() {
-  return await Movie.find().sort({ createdAt: -1 });  
+  try{
+     return await Movie.find().sort({ createdAt: -1 }); 
+  }catch (err){
+     throw new Error("Erro ao buscar filme");
+  }
+   
 }
 
-// listar por gênero
+// listar por 
 async function getMovieByGenre(genre) {
-    return await Movie.find({
-        genre: genre.toLowerCase(),
-      }).sort({ createdAt: -1 });
-}
+  try {
+    const movie = await Movie.find({
+      genre: genre.toLowerCase(),
+    }).sort({ createdAt: -1 });
 
+    if (!movie || movie.length === 0) {
+      throw new Error("Nenhum filme encontrado para esse gênero");
+    }
+
+    return movie;
+
+  } catch (error) {
+    console.error(error);
+    throw new Error("Erro ao buscar filmes por gênero");
+  }
+}
 // buscar por id
 async function getMovieById(id) {
-   return await Movie.findById(id);
+  try{
+    const movie = await Movie.findById(id);
+
+    if (!movie) {
+      throw new Error("Filme não encontrada");
+    }
+
+    return movie;
+
+ }catch (error) {
+    throw new Error("Erro ao buscar filme");
+  } 
 }
 
 // atualizar
 async function updateMovie (id,data) {
- return await Movie.findByIdAndUpdate(id, data, {
+  try{
+    const movie =await Movie.findByIdAndUpdate(id, data, {
      new: true,
      runValidators: true,
    }); 
+
+   if (!movie){
+     throw new Error("Filme não encontrado");
+   }
+   return movie
+
+  }catch (error){
+     throw new Error("Erro ao atualizar filme");
+  }
 }
 
 // deletar
 async function deleteMovie(id) {
-  await Movie.findByIdAndDelete(id);
-    return "Filme deletado com sucesso";
+  try{
+    const movie = await Movie.findByIdAndDelete(id);
+
+    if(!movie){
+      throw new Error("Filme não encontrado");
+    }
+
+    return "Filme deletado com sucesso"
+    
+  }catch (error) {
+    throw new Error("Erro ao deletar filme");
+  }
 }
 
 module.exports = {
